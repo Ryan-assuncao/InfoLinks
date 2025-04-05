ab80a3e711ec4b4008f57ff1379d1479e34271e4
Ryan Assunção <ryansilvaassuncao1@gmail.com>
2025-04-05 14:22:48 -0400

    inserindo novos elementos

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,11 @@
 import { Text, View } from "react-native"
 export default function Index () {
     return (
+        // Export default é o metodo usado para dizer ao ExporRouter que esse componente é uma Rota.
         <View>
-        <Text> eu sou o mior</Text>
+        <Text> eu sou o milhor</Text>
+        <Text>em minha cabeça eu sou o milhor</Text>
+        <Text>luis espalha</Text>
         </View>
         
     );
