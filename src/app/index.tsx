101184478d0f6eb098b9954d61ed569b447316b7
Ryan Assunção <ryansilvaassuncao1@gmail.com>
2025-04-11 19:42:18 -0400

    novos elementos

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,19 +2,26 @@ import { Text, View, StyleSheet } from "react-native"
 export default function Index () {
     return (
         // Export default é o metodo usado para dizer ao ExporRouter que esse componente é uma Rota.
-        <View>
+        <View style={Styles.container} >
         <Text style={{ color: "red", fontSize: 22}}> eu sou o milhor </Text>
         <Text style={{color: "gray", fontSize: 30}}>em minha cabeça eu sou o milhor </Text>
         <Text style={Styles.title}>luis espalha </Text>
         <Text style={Styles.title2}>fernadinho beira mar </Text>
         <Text style={Styles.title3}>que fase ta o curinthians </Text>
         <Text style={Styles.title4}>flamengo melhor do mundo</Text>
+        <Text style={Styles.title5}>vem pro ifam</Text>
         </View>
         
     );
 }
 const Styles = StyleSheet.create({
    
+    container:{
+        flex: 1,
+        justifyContent: "center",
+        alignItems:"center",
+        flexDirection:"row",
+    },
 
     title:{
         color: "blue",
@@ -34,5 +41,11 @@ const Styles = StyleSheet.create({
     title4:{
         color: "red",
         fontSize: 28
-    }
+    },
+
+    title5:{
+        color: "green",
+        fontSize: 28
+    },
+    
 })
